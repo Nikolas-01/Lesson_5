ec29ab702d133a94e862201368343ca9f99a71a9
ПК <89068414983@bk.ru>
2020-04-01 21:56:51 +0500

    коммит домашки

--- 5.py
+++ 5.py
@@ -1,4 +1,4 @@
-# 1) проверка числа на простоту. Все функции модуля принимают на вход натуральные числа от 1 до 1000.
+# 1) проверка числа на простоту.
 
 from math import sqrt
 
@@ -12,22 +12,10 @@ def num_simple(n):
         i += 1
     return True
 
-n = int(input('Input natural number '))
-print('1) проверка числа на простоту\n', num_simple(n))
+n = int(input('Введите натуральное число '))
+print('1) проверяем на простоту\n', num_simple(n))
 
-# 2) выводит список всех делителей числа;
-# def num_divisors(n):
-#     result = []
-#     i = 1
-#     while i <= sqrt(n):
-#         if n % i == 0:
-#             if (n / i) % i == i:
-#                 result.append(i)
-#             else:
-#                 result.extend([i, n // i])
-#         i += 1
-#     result.sort()
-#     return result
+5
 def num_divisors(n):
     result = []
     for i in range(1, n + 1):
@@ -36,13 +24,11 @@ def num_divisors(n):
     return result
 
 
-print('2) выводит список всех делителей числа\n', num_divisors(n))
+print('2) Вывод списка делителей числа\n', num_divisors(n))
 
-# 3) выводит самый большой простой делитель числа.
 def num_max_divisor(n):
     if n == 1:
         return None
     return max(list(filter(num_simple, num_divisors(n))))
 
-
-print('3) выводим самый большой простой делитель числа\n', num_max_divisor(n))
+print('3) Вывод самого большого простого делителя числа\n', num_max_divisor(n))
