d2e674e831cc372cd4d3d73fabeece0bd04ce5ec
ПК <89068414983@bk.ru>
2020-04-02 11:18:21 +0500

    коммит

--- 5.py
+++ 5.py
@@ -1,7 +1,5 @@
-# 1) проверка числа на простоту.
-
+# 1. проверка числа на простоту.
 from math import sqrt
-
 def num_simple(n):
     if n <= 1:
         return False
@@ -11,10 +9,8 @@ def num_simple(n):
             return False
         i += 1
     return True
-
 n = int(input('Введите натуральное число '))
 print('1) проверяем на простоту\n', num_simple(n))
-
 5
 def num_divisors(n):
     result = []
@@ -22,13 +18,9 @@ def num_divisors(n):
         if n % i == 0:
             result.append(i)
     return result
-
-
 print('2) Вывод списка делителей числа\n', num_divisors(n))
-
 def num_max_divisor(n):
     if n == 1:
         return None
     return max(list(filter(num_simple, num_divisors(n))))
-
 print('3) Вывод самого большого простого делителя числа\n', num_max_divisor(n))
